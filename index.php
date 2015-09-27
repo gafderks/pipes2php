508221e51ee16832ae624187e2900ddd8e800188
Geert Derks <g.a.f.derks@student.tue.nl>
2015-09-27 20:41:21 +0200

    Created generic index.php.

--- index.php
+++ index.php
@@ -1,113 +1,49 @@
-<?php
-
-header('Content-type: application/xml');
+<?php // /index.php
 
 require 'bootstrap.php';
 
+/**
+ * 
+ */
+define("SCRIPT_DIR", __DIR__ .  DIRECTORY_SEPARATOR . "pipes" . DIRECTORY_SEPARATOR);
 
 $app = new \Slim\Slim();
-$app->get("/test", function() {
-   
-    $ff = new \Module\FetchFeed(array("URL" => "https://www.google.com/calendar/feeds/im9dkimg71lmvd5iu0461fkkcs%40group.calendar.google.com/public/basic?futureevents=true&orderby=starttime&sortorder=a&language=nl_NL&country=NL&singleevents=true&hl=nl"));
+
+/**
+ * 
+ */
+$app->get("/:pipe", function($pipe) use ($app) {
     
-    $rn = new \Module\Rename(
-        array("rules" =>
-            array(
-                array(
-                    "field" => "description",
-                    "op" => "copy",
-                    "newval" => "summary"
-                )
-            )
-        )
-    );
-    $rn->in($ff->out());
+    // escape pipe name
+    $pipeName = preg_replace('/[^A-Za-z0-9_\-]/', '_', $pipe);
     
-    $re = new \Module\RegEx(
-        array("rules" => 
-            array(
-                array(
-                    "field" => "description",
-                    "match" => "Status van afspraak: .*",
-                    "replace" => ""
-                ),
-                array(
-                    "field" => "description",
-                    "match" => "Wie: .*",
-                    "replace" => ""
-                ),
-                array(
-                    "field" => "description",
-                    "match" => "Wanneer: ",
-                    "replace" => ""
-                ),
-                array(
-                    "field" => "description",
-                    "match" => "Waar:",
-                    "replace" => ""
-                ),
-                array(
-                    "field" => "description",
-                    "match" => "Beschrijving van afspraak: .*",
-                    "replace" => ""
-                ),
-                array(
-                    "field" => "description",
-                    "match" => "\n",
-                    "replace" => "",
-                    "globalmatch" => true,
-                    "multilinematch" => true
-                ),
-                array(
-                    "field" => "description",
-                    "match" => "\r",
-                    "replace" => "",
-                    "globalmatch" => true,
-                    "multilinematch" => true
-                ),
-                array(
-                    "field" => "description",
-                    "match" => "CET",
-                    "replace" => "",
-                    "globalmatch" => true
-                ),
-                array(
-                    "field" => "description",
-                    "match" => "(\d)\.",
-                    "replace" => "$1",
-                    "globalmatch" => true
-                ),
-                array(
-                    "field" => "title",
-                    "match" => "Verk: ",
-                    "replace" => ""
-                ),
-                array(
-                    "field" => "description",
-                    "match" => "CEST",
-                    "replace" => "",
-                    "globalmatch" => true
-                ),
-                array(
-                    "field" => "description",
-                    "match" => "<br>",
-                    "replace" => "",
-                    "globalmatch" => true,
-                    "multilinematch" => true
-                ),
-                array(
-                    "field" => "description",
-                    "match" => "&nbsp;",
-                    "replace" => "",
-                    "globalmatch" => true
-                )
-            )
-        )
-    );
-    $re->in($rn->out());
+    // check if pipe definition exists
+    if (!file_exists(SCRIPT_DIR . $pipeName . ".json")) {
+        // show 404
+        $app->notFound();
+        die();
+    }
     
-    echo $re->out()->asXML();
+    // retrieve definition
+    $pipeDefinition = json_decode(file_get_contents(SCRIPT_DIR . $pipeName . ".json"));
     
+    // create pipe evaluator
+    $ev = new \Common\Evaluator($pipeDefinition);
+    
+    // run evaluator
+    $result = $ev->evaluate();
+    
+    // define content-type
+    header('Content-type: application/xml');
+    
+    // format result
+    $dom = new DOMDocument("1.0");
+    $dom->preserveWhiteSpace = false;
+    $dom->formatOutput = true;
+    $dom->loadXML($result->asXML());
+        
+    // output
+    echo $dom->saveXML();
     
 });
 $app->run();
